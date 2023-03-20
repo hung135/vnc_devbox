f590380c020ae7a53ff39ab77751f346ff6e03e5
hung135 <hung135@hotmail.com>
2023-03-20 03:39:26 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,14 +4,51 @@
 mkdir -p /root/.vnc
 
 # Set a password for the VNC server (optional)
-echo "mypassword" | vncpasswd -f > /root/.vnc/passwd
+if [ -z "$VNC_PASSWORD" ]; then
+  echo "No VNC password provided, using default password 'password'"
+  echo "password" | vncpasswd -f > /root/.vnc/passwd
+else
+  echo "Setting VNC password..."
+  echo "$VNC_PASSWORD" | vncpasswd -f > /root/.vnc/passwd
+fi
 chmod 600 /root/.vnc/passwd
 
-# Start the VNC server with the specified resolution
-VNC_RESOLUTION=${VNC_RESOLUTION:-1280x720}
-vncserver :1 -geometry $VNC_RESOLUTION -depth 24 -localhost no
+# Start the VNC server
+vncserver :1 -geometry "$VNC_RESOLUTION" -depth 24 -localhost no
 
-# Start a terminal session within the VNC environment
-export DISPLAY=:1
-xterm -geometry 800x600 &
+# Prompt the user for which application to run
+while true; do
+  clear
+  echo "Choose an application to run:"
+  echo "1. Terminal"
+  echo "2. Transmission"
+  echo "3. NetBeans"
+  echo "4. Firefox-esr"
+  read -p "Enter your choice (1-4): " choice
+
+  case $choice in
+    1)
+      xterm -geometry "$VNC_RESOLUTION" &
+      break
+      ;;
+    2)
+      transmission-gtk &
+      break
+      ;;
+    3)
+      /opt/netbeans/bin/netbeans &
+      break
+      ;;
+    4)
+      firefox-esr &
+      break
+      ;;
+    *)
+      echo "Invalid choice, please try again..."
+      sleep 2
+      ;;
+  esac
+done
+
+# Keep the container running
 tail -f /dev/null
