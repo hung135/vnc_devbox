817625ca4d55df77c7eaa220d1c3ac0514210f34
hung135 <hung135@hotmail.com>
2023-03-20 03:41:16 -0400

    Create start_menu.sh

---
+++ start_menu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+while true; do
+  clear
+  echo "Choose an application to run:"
+  echo "1. Terminal"
+  echo "2. Transmission"
+  echo "3. NetBeans"
+  echo "4. Firefox-esr"
+  read -p "Enter your choice (1-4): " choice
+
+  case $choice in
+    1)
+      xterm -geometry "$VNC_RESOLUTION" &
+      break
+      ;;
+    2)
+      transmission-gtk &
+      break
+      ;;
+    3)
+      /opt/netbeans/bin/netbeans &
+      break
+      ;;
+    4)
+      firefox-esr &
+      break
+      ;;
+    *)
+      echo "Invalid choice, please try again..."
+      sleep 2
+      ;;
+  esac
+done
