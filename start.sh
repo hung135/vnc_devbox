38b20465665ca81e75eb5dd07affaeb2ddb5d160
hung135 <hung135@hotmail.com>
2023-03-20 03:41:34 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,39 +16,9 @@ chmod 600 /root/.vnc/passwd
 # Start the VNC server
 vncserver :1 -geometry "$VNC_RESOLUTION" -depth 24 -localhost no
 
-# Prompt the user for which application to run
-while true; do
-  clear
-  echo "Choose an application to run:"
-  echo "1. Terminal"
-  echo "2. Transmission"
-  echo "3. NetBeans"
-  echo "4. Firefox-esr"
-  read -p "Enter your choice (1-4): " choice
-
-  case $choice in
-    1)
-      xterm -geometry "$VNC_RESOLUTION" &
-      break
-      ;;
-    2)
-      transmission-gtk &
-      break
-      ;;
-    3)
-      /opt/netbeans/bin/netbeans &
-      break
-      ;;
-    4)
-      firefox-esr &
-      break
-      ;;
-    *)
-      echo "Invalid choice, please try again..."
-      sleep 2
-      ;;
-  esac
-done
+# Start a terminal session within the VNC environment
+export DISPLAY=:1
+xterm -geometry "$VNC_RESOLUTION" -e "/start-menu.sh" &
 
 # Keep the container running
 tail -f /dev/null
